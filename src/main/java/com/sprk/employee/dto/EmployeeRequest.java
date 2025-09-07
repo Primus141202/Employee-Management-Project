c0fe21efe9f0e5e26ec1742a10edaf66a07a7547
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-07 13:23:20 +0530

    Update EmployeeRequest.java

--- src/main/java/com/sprk/employee/dto/EmployeeRequest.java
+++ src/main/java/com/sprk/employee/dto/EmployeeRequest.java
@@ -1,6 +1,7 @@
 package com.sprk.employee.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public class EmployeeRequest {
@@ -11,9 +12,19 @@ public class EmployeeRequest {
     @NotBlank(message = "Last name is required")
     private String lastName;
 
+    @Min(value = 18, message = "Age must be at least 18")
+    private int age;
+
+    @NotBlank(message = "Gender is required")
+    private String gender;
+
+    @Min(value = 10000, message = "Salary must be>=10000")
+    private double salary;
+
     @Email(message = "Invalid Email")
     @NotBlank(message = "Email is required")
     private String email;
-    
+
+    @NotBlank(message = "Department Id is required")
     private long departmentID;
 }
