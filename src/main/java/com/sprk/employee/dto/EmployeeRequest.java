d1d74c3dde210f8ec67b08a472d8ac8ae658c2f2
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-07 13:15:58 +0530

    Created Employee Request DTO

--- src/main/java/com/sprk/employee/dto/EmployeeRequest.java
+++ src/main/java/com/sprk/employee/dto/EmployeeRequest.java
@@ -1,4 +1,19 @@
 package com.sprk.employee.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class EmployeeRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @Email(message = "Invalid Email")
+    @NotBlank(message = "Email is required")
+    private String email;
+    
+    private long departmentID;
 }
