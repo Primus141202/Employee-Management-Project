3345c93ddeca0bde10dea20a8579c7f498dc40e5
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-07 12:58:55 +0530

    Created Employee Entity

--- src/main/java/com/sprk/employee/entity/Employee.java
+++ src/main/java/com/sprk/employee/entity/Employee.java
@@ -1,13 +1,31 @@
 package com.sprk.employee.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 public class Employee {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)private Integer id;
 
     @Column(nullable=false, length=64)
+    private String firstName;
+
+    @Column(nullable = false,length = 64)
+    private String lastName;
+
+    private int age;
+
+    @Column(nullable = false,length = 10)
+    private String gender;
+
+    private double salary;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+
+    @JoinColumn(name="department_id")
+    private Department department;
 }
