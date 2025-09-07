cdba335a811cf6f0de8f78b8cde852a90e073785
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-07 13:03:58 +0530

    Created Department Entity

--- src/main/java/com/sprk/employee/entity/Department.java
+++ src/main/java/com/sprk/employee/entity/Department.java
@@ -1,4 +1,19 @@
 package com.sprk.employee.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@Entity
+@AllArgsConstructor
 public class Department {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)private Integer id;
+
+    @Column(nullable = false,length = 32)
+    private String name;
+
+    @Column(nullable = false,length = 128)
+    private String location;
 }
