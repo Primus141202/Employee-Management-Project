c86946f2e5f7e238ed2e2aa08ad143d60fa4dbe6
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-09 16:27:51 +0530

    Created Department Service

--- src/main/java/com/sprk/employee/service/DepartmentService.java
+++ src/main/java/com/sprk/employee/service/DepartmentService.java
@@ -1,4 +1,8 @@
 package com.sprk.employee.service;
 
+import com.sprk.employee.entity.Department;
+
 public interface DepartmentService {
+    Department getOrCreate(String code, String nameIfCreate);
+    Department getByCodeorThrow(String code);
 }
