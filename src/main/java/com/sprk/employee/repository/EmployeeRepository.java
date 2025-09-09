25f0707935f4c457eef7a6ef20e0505671d5e213
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-09 16:25:51 +0530

    Created Employee Repository

--- src/main/java/com/sprk/employee/repository/EmployeeRepository.java
+++ src/main/java/com/sprk/employee/repository/EmployeeRepository.java
@@ -1,4 +1,21 @@
 package com.sprk.employee.repository;
 
-public interface EmployeeRepository {
+import com.sprk.employee.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    boolean existsByEmail(String email);
+
+    //Filtering with pagination
+    Page<Employee>
+    findByGender(String gender, Pageable pageable);
+    Page<Employee>
+    findByDepartment_Code(String departmentCode, Pageable pageable);
+
+
+
 }
