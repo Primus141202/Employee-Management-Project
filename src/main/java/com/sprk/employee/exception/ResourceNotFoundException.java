62cb9239f6c2eefb1d92189be7399077c36d4d05
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-10 23:39:26 +0530

    Create ResourceNotFoundException.java

---
+++ src/main/java/com/sprk/employee/exception/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package com.sprk.employee.exception;
+
+public class ResourceNotFoundException extends @org.jetbrains.annotations.NotNull X {
+    public ResourceNotFoundException(String s) {
+    }
+}
