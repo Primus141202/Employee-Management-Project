67d35e51c55c3bd123da0942ed44a5ded28b61cd
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-10 23:49:13 +0530

    Created EmployeeService

--- src/main/java/com/sprk/employee/service/EmployeeService.java
+++ src/main/java/com/sprk/employee/service/EmployeeService.java
@@ -1,4 +1,15 @@
 package com.sprk.employee.service;
 
+import com.sprk.employee.dto.EmployeeRequest;
+import com.sprk.employee.dto.EmployeeResponse;
+import org.springframework.data.domain.Page;
+
 public interface EmployeeService {
+    EmployeeResponse create(EmployeeRequest request);
+    EmployeeResponse getById(Integer id);
+    EmployeeResponse update(Integer id,EmployeeRequest request);
+    void delete(Integer id);
+
+    //Optional filters
+    Page<EmployeeResponse>list(Integer page, Integer size, String sortBy, String direction, String gender, String departmentCode);
 }
