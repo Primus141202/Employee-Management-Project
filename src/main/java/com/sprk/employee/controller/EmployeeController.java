7d4ca48a6b4ba2f192e0f9292a58b310adc95130
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-11 23:44:28 +0530

    Update EmployeeController.java

--- src/main/java/com/sprk/employee/controller/EmployeeController.java
+++ src/main/java/com/sprk/employee/controller/EmployeeController.java
@@ -1,4 +1,55 @@
 package com.sprk.employee.controller;
 
+import com.sprk.employee.dto.EmployeeRequest;
+import com.sprk.employee.dto.EmployeeResponse;
+import com.sprk.employee.service.EmployeeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Pageable;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeResponse> createEmployee(@Valid @RequestBody EmployeeRequest request) {
+        EmployeeResponse created = employeeService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EmployeeResponse>> getAllEmployees(
+            @RequestParam(required = false) String department,
+            @PageableDefault(size = 10, sort = "id") Pageable pageable) {
+        Page<EmployeeResponse> employees = employeeService.getAll(department, pageable);
+        return ResponseEntity.ok(employees);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeResponse> getEmployeeById(@PathVariable Long id) {
+        EmployeeResponse response = employeeService.getById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeResponse> updateEmployee(
+            @PathVariable Long id,
+            @Valid @RequestBody EmployeeRequest request) {
+        EmployeeResponse updated = employeeService.updateEmployee(id, request);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        employeeService.delete(id);
+        return ResponseEntity.noContent().build(); // 204 No Content
+    }
 }
