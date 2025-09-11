1bcbb84497448e0d62a828bccf00b381a82fb830
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-11 23:43:30 +0530

    Update DepartmentService.java

--- src/main/java/com/sprk/employee/service/DepartmentService.java
+++ src/main/java/com/sprk/employee/service/DepartmentService.java
@@ -5,4 +5,16 @@ import com.sprk.employee.entity.Department;
 public interface DepartmentService {
     Department getOrCreate(String code, String nameIfCreate);
     Department getByCodeorThrow(String code);
+    DepartmentResponse create(DepartmentRequest request);
+
+    // Get a list of all departments as DTOs
+    List<DepartmentResponse> getAll();
+
+    // Get a single department by ID as DTO
+    DepartmentResponse getById(Long id);
+
+    // Optionally, if you want to add update and delete
+    DepartmentResponse update(Long id, DepartmentRequest request);
+
+    void delete(Long id);
 }
