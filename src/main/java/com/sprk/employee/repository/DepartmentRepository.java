e65447d16184d3f2246c86ea4f208744b24b59ad
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-12 14:58:01 +0530

    Update DepartmentRepository.java

--- src/main/java/com/sprk/employee/repository/DepartmentRepository.java
+++ src/main/java/com/sprk/employee/repository/DepartmentRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface DepartmentRepository extends JpaRepository {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
     Optional<Department> findByCode(String code);
+
     boolean existsByCode(String code);
 }
