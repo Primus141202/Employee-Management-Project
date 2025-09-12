b8960eaf63318f4c45607dd706e9511b1f22682e
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-12 14:20:11 +0530

    Create ApiException.java

---
+++ src/main/java/com/sprk/employee/exception/ApiException.java
@@ -0,0 +1,14 @@
+package com.sprk.employee.exception;
+
+public class ApiException {
+    private final int statusCode;
+
+    public ApiException(String message, int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
