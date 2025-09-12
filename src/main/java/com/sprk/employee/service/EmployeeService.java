13f77c2d753a8f687420d72424e75efcbb384ded
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-12 14:46:00 +0530

    Update EmployeeService.java

--- src/main/java/com/sprk/employee/service/EmployeeService.java
+++ src/main/java/com/sprk/employee/service/EmployeeService.java
@@ -3,13 +3,20 @@ package com.sprk.employee.service;
 import com.sprk.employee.dto.EmployeeRequest;
 import com.sprk.employee.dto.EmployeeResponse;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface EmployeeService {
+
     EmployeeResponse create(EmployeeRequest request);
+
     EmployeeResponse getById(Integer id);
-    EmployeeResponse update(Integer id,EmployeeRequest request);
+
+    EmployeeResponse update(Integer id, EmployeeRequest request);
+
     void delete(Integer id);
 
-    //Optional filters
-    Page<EmployeeResponse>list(Integer page, Integer size, String sortBy, String direction, String gender, String departmentId);
+    Page<EmployeeResponse> getAll(String department, Pageable pageable);
+
+    Page<EmployeeResponse> list(Integer page, Integer size, String sortBy, String direction,
+                                String gender, String departmentCode);
 }
