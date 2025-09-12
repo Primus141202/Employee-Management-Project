80e89ff4e204d3b4cee763d3d88247cf193b345e
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-12 14:23:24 +0530

    Update EmployeeController.java

--- src/main/java/com/sprk/employee/controller/EmployeeController.java
+++ src/main/java/com/sprk/employee/controller/EmployeeController.java
@@ -3,20 +3,21 @@ package com.sprk.employee.controller;
 import com.sprk.employee.dto.EmployeeRequest;
 import com.sprk.employee.dto.EmployeeResponse;
 import com.sprk.employee.service.EmployeeService;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
-
 @RestController
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
+
     private final EmployeeService employeeService;
 
     @PostMapping
@@ -34,22 +35,22 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EmployeeResponse> getEmployeeById(@PathVariable Long id) {
+    public ResponseEntity<EmployeeResponse> getEmployeeById(@PathVariable Integer id) {
         EmployeeResponse response = employeeService.getById(id);
         return ResponseEntity.ok(response);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<EmployeeResponse> updateEmployee(
-            @PathVariable Long id,
+            @PathVariable Integer id,
             @Valid @RequestBody EmployeeRequest request) {
-        EmployeeResponse updated = employeeService.updateEmployee(id, request);
+        EmployeeResponse updated = employeeService.update(id, request);
         return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Integer id) {
         employeeService.delete(id);
-        return ResponseEntity.noContent().build(); // 204 No Content
+        return ResponseEntity.noContent().build();
     }
 }
